3c7d3814c7dea9061b5c6c8e0227c6a027166a2a
yuhere <yuhere@43e9b134-f3b9-11de-91ba-91c70a792394>
2009-12-30 03:40:21 +0000

    修改安装程序不能安装数据的问题

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,22 +1,38 @@
 # -*- coding: utf-8 -*- 
-from distutils.core import setup
+
 from setuptools import setup, find_packages
+import os
+import sys
+
+def find_pyerp_data():
+    data_files = []
+    root_dir = os.path.dirname(__file__)
+    if root_dir != '':
+        os.chdir(root_dir)
+    src_dir = 'pyerp'
+    for dirpath, dirnames, filenames in os.walk(src_dir):
+        # Ignore dirnames that start with '.'
+        for i, dirname in enumerate(dirnames):
+            if dirname.startswith('.'): del dirnames[i]
+        for f in filenames:
+            if not f.endswith('.py') and not f.endswith('.pyc'):
+                data_files.append(os.path.join(dirpath[len(src_dir)+1:], f))
+    return data_files
 
 version = __import__('pyerp').get_version()
-
 
 setup(
     name = 'Pyerp', 
     version = version, 
+    url = 'http://www.pyerp.cn/', 
+    download_url = 'http://www.pyerp.cn/download/',
+    author = 'Pyerp Software', 
+    author_email = 'yuhere@gmail.com', 
     description = 'Integrated Financial CRM', 
     long_description = "Pyerp is a web-based software enterprise resource planning system. " \
                        "It provides an application framework and some plug-ins application." \
                        "ex. Financial application , CRM application, .",
-    author = 'Pyerp Software', 
-    author_email = 'yuhere@gmail.com', 
     license = 'GPLv3', 
-    url = 'http://www.pyerp.cn/', 
-    download_url = 'http://www.pyerp.cn/download/', 
     classifiers = [ 
         'Environment :: Web Environment', 
         'Framework :: Pyerp ' + version, 
@@ -27,12 +43,12 @@ setup(
         'Topic :: Software Development :: ERP System', 
         'Topic :: Software Development :: Financial CRM', 
     ], 
-    # package_dir={'pyerp': 'src/pyerp'},
-    # packages=find_packages(exclude=['*.tests']),
-    packages=find_packages(exclude=['tests**']),
+    packages = find_packages(exclude=['tests**']),
+    package_data = {'pyerp' : find_pyerp_data()},
     test_suite = 'tests.suite',
-    #~ install_requires = [
-        #~ 'Django>=1.0.2_final',
-        #~ 'Genshi>=0.5'
-    #~ ],
+    install_requires = [
+        'setuptools>=0.6b1',
+#        'Django>=1.0.2',
+    ],
+
   )
