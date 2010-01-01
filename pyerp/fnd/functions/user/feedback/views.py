bf8fcd41417f3c58bb02751b5b12d481c83a0e23
yuhere <yuhere@43e9b134-f3b9-11de-91ba-91c70a792394>
2010-01-01 12:48:44 +0000

    调整[反馈]程序,问题描述错位问题

--- pyerp/fnd/functions/user/feedback/views.py
+++ pyerp/fnd/functions/user/feedback/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*- 
 
+import urllib
 
 from django.http import HttpResponseRedirect
 from django.conf import settings
@@ -8,38 +9,33 @@ from pyerp.fnd.shortcuts import fnd_render_to_response
 from pyerp.fnd.gbl import fnd_global
 from pyerp.fnd import models as fnd_models
 from pyerp.fnd.profile import fnd_profile
-import urllib
 
 
 def display_main(request, func_id=None):
-    # 判断用户是否设定了缺省主页
+    
     fun = fnd_models.Function.objects.get(pk=func_id)
-
     req = urllib.urlencode({"reporter"    : fnd_global.user.username,
           "summary"     : "",
           "type"        : "缺陷",
-          "description" : """功能: %s \n
-功能描述: %s \n
-版本: %s \n
-模块: %s \n
-问题描述: \n""" % (fun.name.encode("utf-8"), 
-                fun.package.encode("utf-8"),
-                fun.description.encode("utf-8"), 
-                fun.version),
+          "description" : "功能: %s \n"     \
+                          "功能描述: %s \n" \
+                          "版本: %s \n"     \
+                          "模块: %s \n"     \
+                          "问题描述: \n" % (
+                           fun.name.encode("utf-8"), 
+                           fun.description.encode("utf-8"),
+                           fun.version, 
+                           fun.package.encode("utf-8"),
+                           ),
 #          "action"      : "create",
 #          "status"      : "new",
           "priority"    : "major",
           "milestone"   : "Pilot",
           "component"   : "fnd",
-          "version"     : "1.0",
+          "version"     : __import__('pyerp').get_version(),
           "keywords"    : "",
           "cc"          : ""
         })
-    
-#    from pyerp.fnd.api import request as fnd_request
-#    fnd_request.submit_request(1)
-    
-    return HttpResponseRedirect("http://code.pyerp.cn/newticket?" + req)
-    #return fnd_render_to_response('user/feedback/index.html', context, request)
 
+    return HttpResponseRedirect("http://code.pyerp.cn/newticket?" + req)
 
