f77e8c9d226d5930122e2b40f0e6b5302cbf44f4
yuhere <yuhere@43e9b134-f3b9-11de-91ba-91c70a792394>
2010-01-05 13:23:27 +0000

    为设定所有PY程序设定svn关键字Id.

--- pyerp/fnd/utils/version.py
+++ pyerp/fnd/utils/version.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*- 
 
+__svnid__ = '$Id:$'
+
+import re
+
+def get_svn_id(_svnid):
+    # bow for the mighty regular expression
+    svnidrep = r'^\$Id: (?P<filename>.+) (?P<revision>\d+) (?P<last_changed_date>\d{4}-\d{2}-\d{1,2}) (?P<last_changed_time>\d{2}:\d{2}:\d{2})Z (?P<author>\w+.+) \$$'
+    # parse the svn Id
+    return  re.match(svnidrep, _svnid).group('last_changed_date', 'author')
 
 def get_svn_revision(path):
     """
