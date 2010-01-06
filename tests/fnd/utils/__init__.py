4e33d1bc7bf13b8e9b24e1a40075cfd686335660
yuhere <yuhere@43e9b134-f3b9-11de-91ba-91c70a792394>
2010-01-06 15:25:37 +0000

    添加fnd.utils.version测试

--- tests/fnd/utils/__init__.py
+++ tests/fnd/utils/__init__.py
@@ -2,3 +2,4 @@
 
 import unittest
 
+__svnid__ = '$Id: __init__.py 45 2010-01-06 14:35:30Z yuhere $'
--- tests/fnd/utils/version.py
+++ tests/fnd/utils/version.py
@@ -14,6 +14,10 @@ class VersionTestCase(unittest.TestCase):
 
     def test_get_version(self):
         from pyerp.fnd.utils import version
+        ver = version.get_version(0, 0, 1, 'alpha', 0)
+        self.assertEquals('0.0.1-pre-alpha', ver)
+        ver = version.get_version(0, 0, 1, 'final', 0)
+        self.assertEquals('0.0.1-final', ver)
         ver = version.get_version(0, 0, 1, 'dev', 0)
         self.assertEquals('0.0.1-dev-0', ver)
 
@@ -21,5 +25,11 @@ class VersionTestCase(unittest.TestCase):
         from pyerp.fnd.utils import version
         _svn_ = version.get_svn_revision(__name__)
         self.assertEquals(len(_svn_), 3)
+        from tests.fnd import utils
+        _svn_ = version.get_svn_revision(utils.__name__)
+        print _svn_
+        self.assertEquals(len(_svn_), 3)
+        
+        
 
 
