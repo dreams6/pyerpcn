5fefd0e7b17b25f984338a47587c6104ee65fef1
yuhere <yuhere@43e9b134-f3b9-11de-91ba-91c70a792394>
2010-01-07 15:18:58 +0000

    构建Django单元测试环境

--- pyerp/fnd/utils/testrunner.py
+++ pyerp/fnd/utils/testrunner.py
@@ -3,7 +3,12 @@
 
 from bitten.util.testrunner import unittest as b_unittest
 
+
+
 class pyerpunittest(b_unittest):
+    
+    description = b_unittest.description + ', and setup Django unittest evn'
+    
     """
     add follow info into setup.py's setup() option
     entry_points = {
@@ -11,15 +16,38 @@ class pyerpunittest(b_unittest):
             'pyerpunittest = pyerp.fnd.utils.testrunner:pyerpunittest'
         ],
     and run python setup.py egg_info first.(setuptools is required)
-    
+    about django testing: django.test.simple
     """
-    
     # TODO: define option Django's setting
+    user_options = b_unittest.user_options + [
+        ('django-settings=', None, "Django settings module"),
+    ]
     
     
-    
-    
+    def initialize_options(self):
+        b_unittest.initialize_options(self)
+        self.django_settings = 'tests.settings'
+
+    def finalize_options(self):
+        b_unittest.finalize_options(self)
+
     def run_tests(self):
-        print "pyerpunittest.TODO. setup django unittest evn....."
+
+        import os
+        os.environ['DJANGO_SETTINGS_MODULE'] = self.django_settings
+
+        from django.test.utils import setup_test_environment, teardown_test_environment
+        from django.conf import settings
+
+        setup_test_environment()
+        settings.DEBUG = False
+
+        old_name = settings.DATABASE_NAME
+        from django.db import connection
+        connection.creation.create_test_db(1, autoclobber=False)
+        # execute test suites
         b_unittest.run_tests(self)
-        print "pyerpunittest.TODO. distory django unittest evn....."
+
+        connection.creation.destroy_test_db(old_name, 1)
+        teardown_test_environment()
+
