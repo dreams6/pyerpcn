c150f8aefa3eaf9d380745d611cbfa1dfc8b316f
yuhere <yuhere@43e9b134-f3b9-11de-91ba-91c70a792394>
2010-01-31 04:14:47 +0000

    添加修改测试Case

--- trunk/tests/fnd/functions/pub/__init__.py
+++ trunk/tests/fnd/functions/pub/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
-__svnid__ = '$Id: __init__.py 54 2010-01-08 13:14:33Z yuhere $'
+__svnid__ = '$Id$'
 
 
--- trunk/tests/fnd/management/__init__.py
+++ trunk/tests/fnd/management/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*- 
-from pyerp.fnd.utils.version import get_svn_revision, get_version
 __svnid__ = '$Id$'
-__svn__ = get_svn_revision(__name__)
 
 import unittest
 from pyerp.fnd.management import fnd_init, fnd_init_currency, fnd_init_notice, fnd_init_concurrent
