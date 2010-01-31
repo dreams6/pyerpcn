581688ff3b1547a781d9cebab1526f7fd13bb939
yuhere <yuhere@43e9b134-f3b9-11de-91ba-91c70a792394>
2010-01-31 03:27:56 +0000

    修改版本策略

--- trunk/pyerp/fnd/utils/version.py
+++ trunk/pyerp/fnd/utils/version.py
@@ -12,34 +12,33 @@ def _parse_svn_id(_svnid):
         return match_obj.group('revision', 'last_changed_date', 'author')
     else:
         return (None, None, None)
-    
+
 def get_svn_revision(m__name__):
     mod = sys.modules[m__name__]
     m__file__ = mod.__file__
     svn_ret = (None, None, None)
     if hasattr(mod, '__svnid__'):
         svn_ret = _parse_svn_id(mod.__svnid__)
-
+    
     if m__file__.endswith('__init__.py') or m__file__.endswith('__init__.pyc'):
         m__dir__ = os.path.dirname(m__file__)
         for f in os.listdir(m__dir__):
             if not f.startswith('.'):
+                
                 sub_name = None
                 if os.path.isfile(os.path.join(m__dir__, f)):
                     if f.endswith('.py') and f != '__init__.py':
                         sub_name = m__name__ + '.' + f[:-3]
                 elif os.path.isdir(os.path.join(m__dir__, f)):
-                    
                     sub_name = m__name__ + '.' + f
  
                 if sub_name:
-
                     try:
                         sub_mod = __import__(sub_name, {}, {}, [''])
                         svn_sub_mod = (None, None, None)
                         if hasattr(sub_mod, '__svn__'):
                             svn_sub_mod = sub_mod.__svn__
-                        svn_ret_rev = svn_ret[0] or 0
+                        svn_ret_rev = svn_ret[0] and int(svn_ret[0]) or 0
                         svn_sub_mod_rev = svn_sub_mod[0] and int(svn_sub_mod[0]) or 0
                         if svn_sub_mod_rev > svn_ret_rev:
                             svn_ret = svn_sub_mod
@@ -48,18 +47,12 @@ def get_svn_revision(m__name__):
     return svn_ret
 
 def get_version(*args):
-    version = '%s.%s' % (args[0], args[1]) 
-    if args[2]: 
-        version = '%s.%s' % (version, args[2]) 
-    if args[3:5] == ('alpha', 0): 
-        version = '%s-pre-alpha' % version 
-    else: 
+    version = '%s.%s.%s' % args[:3] 
+    if args[3] != 'release':
         version = '%s-%s' % (version, args[3]) 
-        if args[3] != 'final': 
-            version = '%s-%s' % (version, args[4])
-
-    if len(args)>5 and args[5]:
-        version = "%s-SVN-%s" % (version, args[5])
+    if args[3] != 'final': 
+        if len(args)>4 and args[4]:
+            version = "%s-r%s" % (version, args[4][0])
     return version
 
 __svnid__ = '$Id$'
--- trunk/setup.py
+++ trunk/setup.py
@@ -19,7 +19,6 @@ def find_pyerp_data():
                 data_files.append(os.path.join(dirpath[len(src_dir)+1:], f))
     return data_files
 
-
 version = (len(sys.argv)>1 and sys.argv[1]!='pyerpunittest' and __import__('pyerp').__version__) or 'internal-test'
 
 setup(
