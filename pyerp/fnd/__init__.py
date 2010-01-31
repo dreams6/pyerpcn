16c988b5d73d40e51476d1c9db61d24dc96ba9ac
yuhere <yuhere@43e9b134-f3b9-11de-91ba-91c70a792394>
2010-01-31 03:27:56 +0000

    修改版本策略

--- pyerp/fnd/__init__.py
+++ pyerp/fnd/__init__.py
@@ -4,4 +4,5 @@ from pyerp.fnd.utils.version import get_svn_revision, get_version
 
 __svnid__ = '$Id$'
 __svn__ = get_svn_revision(__name__)
-__version__ = get_version(0, 0, 1, 'dev', 0)
+__version__ = get_version(0, 1, 0, 'alpha', __svn__)
+
