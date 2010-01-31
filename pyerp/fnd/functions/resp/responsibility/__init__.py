94342bfbdec7962286a4543037d892e61dadec39
yuhere <yuhere@43e9b134-f3b9-11de-91ba-91c70a792394>
2010-01-31 04:23:57 +0000

    版本策略的调整

--- pyerp/fnd/functions/resp/responsibility/__init__.py
+++ pyerp/fnd/functions/resp/responsibility/__init__.py
@@ -3,4 +3,4 @@ from pyerp.fnd.utils.version import get_svn_revision, get_version
 
 __svnid__ = '$Id$'
 __svn__ = get_svn_revision(__name__)
-__version__ = get_version(0, 0, 1, 'dev', 0, __svn__[0])
+__version__ = get_version(0, 1, 0, 'alpha', __svn__)
--- pyerp/fnd/functions/user/help/urls.py
+++ pyerp/fnd/functions/user/help/urls.py
@@ -4,7 +4,6 @@ from pyerp.fnd.utils.version import get_svn_revision, get_version
 
 __svnid__ = '$Id$'
 __svn__ = get_svn_revision(__name__)
-__version__ = get_version(0, 0, 1, 'dev', 0, __svn__[0])
 
 urlpatterns = patterns('pyerp.fnd.functions.user.help.views',
     (r'^(?P<func_id>\d+)/$'    , 'display_main'),
