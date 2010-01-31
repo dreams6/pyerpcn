e34c412afb366aa77d727ea09b2329e1ea13a980
yuhere <yuhere@43e9b134-f3b9-11de-91ba-91c70a792394>
2010-01-31 10:07:50 +0000

    添加，修改测试Case

--- pyerp/urls.py
+++ pyerp/urls.py
@@ -7,12 +7,6 @@ from pyerp.fnd.sites import respsite, usersite, pubsite, rootsite
 
 urlpatterns = patterns('')
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        ('^' + settings.FND_MEDIA_PREFIX[1:] + '(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'd:/workspace1/ec/media', 'show_indexes': True}),
-    )
-
-
 urlpatterns += patterns('',
     ('^' + settings.FND_RESP_SITE_PREFIX  + '(?P<resp_id>\d+)/(?P<menu_id>\d+)/(?P<function_id>\d+)/(?P<url>.*)', respsite.root),
     ('^' + settings.FND_USER_SITE_PREFIX  + '(?P<url>.*)'                                                       , usersite.root),
