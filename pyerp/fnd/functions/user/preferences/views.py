2b1f0abf4628b2066ab5de38a3e4213aa63f8b90
yuhere <yuhere@43e9b134-f3b9-11de-91ba-91c70a792394>
2010-08-29 09:07:49 +0000

    偏好设定应用功能－初始化

---
+++ pyerp/fnd/functions/user/preferences/views.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*- 
+from django.http import HttpResponseRedirect
+from django.conf import settings
+
+from pyerp.fnd.shortcuts import fnd_render_to_response
+from pyerp.fnd.gbl import fnd_global
+from pyerp.fnd.profile import fnd_profile
+from pyerp.fnd.utils.version import get_svn_revision, get_version
+from pyerp.fnd.models import MailBox
+
+__svnid__ = '$Id$'
+__svn__ = get_svn_revision(__name__)
+
+
+
+def display_main(request, resp_id=None):
+
+    context = {
+        'app_path': request.get_full_path(),
+        'resp_id': resp_id,
+        'last_login': request.session.pop('last_login', None),  # 取出最后登录时间,显示在画面上
+    }
+    return fnd_render_to_response('user/prefs/index.html', context, request)
+
+
