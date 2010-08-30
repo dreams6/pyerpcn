51cbc7b4c24a4a4d72922b223e7df79ed3aaebe8
yuhere <yuhere@43e9b134-f3b9-11de-91ba-91c70a792394>
2010-08-30 12:23:59 +0000

    修改布局

--- pyerp/urls.py
+++ pyerp/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*- 
 
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns
 from django.conf import settings
 
 from pyerp.fnd.sites import respsite, usersite, pubsite, rootsite
